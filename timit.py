52ba486a9d9c44c60a64c5137ff647016ddc1bac
csj6172 <77380115+csj6172@users.noreply.github.com>
2021-11-25 11:33:17 +0900

    Update timit.py
    buckeye data 전용입니다.

--- timit.py
+++ timit.py
@@ -3,16 +3,17 @@ import os
 import shutil
 
 
-phn_file =glob.glob('./**/*.PHN',recursive=True)
-wav_file =glob.glob('./**/*.WAV.wav',recursive=True)
+phn_file =glob.glob('./data/*.phn',recursive=True)
+wav_file =glob.glob('./data/*.wav',recursive=True)
 for i,v in enumerate(phn_file):
   w = wav_file[i]
   if i<(len(phn_file)*8)//10+1:
-    shutil.move(v,os.path.join('../SegFeat/timit_directory/train',os.path.basename(v).replace('PHN','phn')))
-    shutil.move(w,os.path.join('../SegFeat/timit_directory/train',os.path.basename(w)))
-  elif i<(len(phn_file)*9)//10+1:
-    shutil.move(v,os.path.join('../SegFeat/timit_directory/test',os.path.basename(v).replace('PHN','phn')))
-    shutil.move(w,os.path.join('../SegFeat/timit_directory/train',os.path.basename(w)))
+    shutil.move(v,os.path.join('../buckeye_directory/train',os.path.basename(v)))
+    shutil.move(w,os.path.join('../buckeye_directory/train',os.path.basename(w)))
+  #elif i<(len(phn_file)*9)//10+1:
   else:
-    shutil.move(v,os.path.join('../SegFeat/timit_directory/val',os.path.basename(v).replace('PHN','phn')))
-    shutil.move(w,os.path.join('../SegFeat/timit_directory/train',os.path.basename(w)))
+    shutil.move(v,os.path.join('../buckeye_directory/test',os.path.basename(v)))
+    shutil.move(w,os.path.join('../buckeye_directory/test',os.path.basename(w)))
+#  else:
+#    shutil.move(v,os.path.join('../SegFeat/timit_directory/val',os.path.basename(v).replace('PHN','phn')))
+#    shutil.move(w,os.path.join('../SegFeat/timit_directory/train',os.path.basename(w)))
